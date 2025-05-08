95194697362ef158d1dc6b7fa2ad3d8542227d68
Orion <89260997+orionbell@users.noreply.github.com>
2025-05-08 21:21:18 +0300

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,29 +1,37 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Yrnu docs",
-  description: "Yrnu documentation",
-  base: "/yrnu/",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+    title: "yrnu docs",
+    base: "/yrnu/",
+    themeConfig: {
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "Examples", link: "/examples" },
+        ],
+        sidebar: [
+            {
+                text: "Pages",
+                items: [
+                    { text: "Installation", link: "/installation" },
+                    {
+                        text: "Lua",
+                        link: "/lua",
+                        items: [
+                            { text: "Core Utils", link: "/lua_core" },
+                            { text: "Config Utils", link: "/lua_config" },
+                            { text: "Yrnu global", link: "/yrnu_global" },
+                            { text: "Creating a Plugin", link: "/lua_plugin" },
+                            { text: "Creating a library", link: "/lua_lib" },
+                        ],
+                    },
+                    { text: "Cli Usage", link: "/cli_usage" },
+                    { text: "Avilable Plugins", link: "/plugins" },
+                    { text: "Avilable Libraries", link: "/libs" },
+                ],
+            },
+        ],
+        socialLinks: [
+            { icon: "github", link: "https://github.com/orionbell/yrnu" },
+        ],
+    },
+});
